71877472816b2463e4bf7500815bec5d069dc79b
debnathk <debnathk1997@gmail.com>
2025-08-03 22:57:45 -0400

    Error: Create env using conda

---
+++ src/adpy/docking.py
@@ -0,0 +1,38 @@
+# Run docking
+from vina import Vina
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--ligand', required=True, help='Set the ligand path')
+parser.add_argument('--receptor', required=True, help='Set the receptor path')
+
+args = parser.parse_args()
+
+v = Vina(sf_name='vina')
+
+# Set the receptor and ligand from PDBQT files
+ligand = args.ligand
+receptor = args.receptor
+
+# ligand = './prepared_ligands/example_prepared_ligand.pdbqt'
+# receptor = './prepared_proteins/example_prepared_protein.pdbqt'
+
+v.set_receptor(receptor)
+print(f'Receptor: {receptor}')
+
+v.set_ligand_from_file(ligand)
+print(f'Ligand: {ligand}')
+v.compute_vina_maps(center=[-.319, 5.27, 1.59], box_size=[80, 80, 80])
+
+# Score the current pose
+energy = v.score()
+print('Score before minimization: %.3f (kcal/mol)' % energy[0])
+
+# Minimized locally the current pose
+energy_minimized = v.optimize()
+print('Score after minimization : %.3f (kcal/mol)' % energy_minimized[0])
+v.write_pose('./results/example_docking.pdbqt', overwrite=True)
+
+# Dock the ligand
+v.dock(exhaustiveness=32, n_poses=5)
+v.write_poses('./results/example_docking.pdbqt', n_poses=5, overwrite=True)
--- src/adpy/docking.sh
+++ src/adpy/docking.sh
@@ -1,37 +1,15 @@
+#!/bin/bash
+
+mkdir -p ./examples/prepared_ligands
+mkdir -p ./examples/prepared_proteins
+mkdir -p ./examples/results
 
 # Prepare ligands
-mk_prepare_ligand.py -i ./ligands/Structure2D_COMPOUND_CID_24978538.sdf -o ./prepared_ligands/ligand_24978538.pdbqt
+mk_prepare_ligand.py -i ./examples/ligands/example_ligand.sdf -o ./examples/prepared_ligands/example_prepared_ligand.pdbqt
 
 # Prepare targets
-mk_prepare_receptor.py -i ./proteins/AF-A5LHX3-F1.pdb -o ./prepared_proteins/AF-A5LHX3-F1_receptor -p -v \
---box_size 80 80 80 --box_center -.3190 5.2703 1.59
+mk_prepare_receptor.py -i ./examples/proteins/example_protein.pdb -o ./examples/prepared_proteins/example_prepared_protein.pdbqt -p -v \
+--box_size 80 80 80 --box_center -.319 5.27 1.59
 
 # Run docking
-from vina import Vina
-
-
-v = Vina(sf_name='vina')
-
-# Set the receptor and ligand from PDBQT files
-ligand = './prepared_ligands/ligand_24978538.pdbqt'
-receptor = './prepared_proteins/AF-A5LHX3-F1_receptor.pdbqt'
-
-v.set_receptor(receptor)
-print(f'Receptor: {receptor}')
-
-v.set_ligand_from_file(ligand)
-print(f'Ligand: {ligand}')
-v.compute_vina_maps(center=[-.319, 5.27, 1.59], box_size=[67, 50, 80])
-
-# Score the current pose
-energy = v.score()
-print('Score before minimization: %.3f (kcal/mol)' % energy[0])
-
-# Minimized locally the current pose
-energy_minimized = v.optimize()
-print('Score after minimization : %.3f (kcal/mol)' % energy_minimized[0])
-v.write_pose('1iep_ligand_minimized.pdbqt', overwrite=True)
-
-# Dock the ligand
-v.dock(exhaustiveness=32, n_poses=5)
-v.write_poses('1iep_ligand_vina_out.pdbqt', n_poses=5, overwrite=True)
+python docking.py --ligand ./examples/prepared_ligands/example_prepared_ligand.pdbqt --receptor ./examples/prepared_proteins/example_prepared_protein.pdbqt
