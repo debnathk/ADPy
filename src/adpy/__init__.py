7cc9d699c3f46f73a357845eb2c2c61fe396018c
debnathk <debnathk1997@gmail.com>
2025-08-04 20:05:03 -0400

    Update repo

--- src/adpy/__init__.py
+++ src/adpy/__init__.py
@@ -1,5 +1,4 @@
-# from .docking import run_docking, prepare_receptor
-from .docking import run_dock
+from .vina import run_dock
 from .utils import log2csv
 
 __all__ = ["log2csv", "run_dock"]
---
+++ src/main.py
@@ -0,0 +1,6 @@
+from adpy import run_dock
+
+ligand = '/Users/debnathk/Documents/ADPy/examples/prepared_ligands/example_prepared_ligand.pdbqt'
+receptor = '/Users/debnathk/Documents/ADPy/examples/prepared_proteins/example_prepared_protein.pdbqt'
+
+run_dock(ligand=ligand, receptor=receptor)
