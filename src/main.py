60ab4389725474b12fd0fae76d10ea0a777817bf
debnathk <debnathk1997@gmail.com>
2025-08-09 15:09:13 -0400

    Modify main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
 from adpy import run_dock
+import argparse
 
-ligand = '/Users/debnathk/Documents/ADPy/examples/prepared_ligands/example_prepared_ligand.pdbqt'
-receptor = '/Users/debnathk/Documents/ADPy/examples/prepared_proteins/example_prepared_protein.pdbqt'
+# ligand = '/Users/debnathk/Documents/ADPy/examples/prepared_ligands/example_prepared_ligand.pdbqt'
+# receptor = '/Users/debnathk/Documents/ADPy/examples/prepared_proteins/example_prepared_protein.pdbqt'
 
-run_dock(ligand=ligand, receptor=receptor)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ligand', required=True, help='Add Ligand Path')
+    parser.add_argument('--receptor', required=True, help='Add Receptor Path')
+    args = parser.parse_args()
+
+    run_dock(ligand=args.ligand, receptor=args.receptor)
